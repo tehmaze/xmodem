02567f6f6309b4516e0f0028142514a5043a3586
maze <maze@pyth0n.org>
2010-04-22 16:58:45 +0200

    Version 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'xmodem',
-    version      = '0.1',
+    version      = '0.2',
     author       = 'Wijnand Modderman',
     author_email = 'maze@pyth0n.org',
     description  = ('XMODEM protocol implementation.'),
