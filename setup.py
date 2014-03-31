8f04331f171ac283b531dc5c9140b93ae7d02fbf
Kris Hardy <kris@rkrishardy.com>
2014-03-30 19:15:41 -0600

    Bumped version number and added xmodem.egg-info and dist to .gitignore

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'xmodem',
-    version      = '0.3.2',
+    version      = '0.3.3',
     author       = 'Wijnand Modderman',
     author_email = 'maze@pyth0n.org',
     description  = ('XMODEM protocol implementation.'),
